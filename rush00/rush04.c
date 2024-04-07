083a4f291b432adb22799bb1eba6f4cd4fc8abde
swojjy <koh0402@gmail.com>
2024-04-07 21:21:24 +0800

    rush00 (04)

---
+++ rush00/rush04.c
@@ -0,0 +1,27 @@
+void	ft_putchar(char c);
+
+void	rush(int x, int y)
+{
+	int	row;
+	int	column;
+
+	row = 1;
+	while (row <= y)
+	{
+		column = 1;
+		while (column <= x)
+		{
+			if ((row == 1 && column == 1) || (row == y && column == x && x > 1 && y > 1))
+				ft_putchar('A');
+			else if ((row == 1 && column == x) || (row == y && column == 1))
+				ft_putchar('C');
+			else if (row == 1 || row == y || column == 1 || column == x)
+				ft_putchar('B');
+			else
+				ft_putchar(' ');
+			column ++;
+		}
+		row ++;
+		ft_putchar('\n');
+	}
+}
