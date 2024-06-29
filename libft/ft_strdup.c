5f585a1b984760eb621634c4ee11134dbb651ab8
Angel Si En Koh <ankoh@c2r3s9.42singapore.sg>
2024-06-29 13:33:39 +0800

    headers

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -1,36 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ankoh <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/29 13:29:04 by ankoh             #+#    #+#             */
-/*   Updated: 2024/06/29 13:29:54 by ankoh            ###   ########.fr       */
+/*   Created: 2024/06/29 13:31:32 by ankoh             #+#    #+#             */
+/*   Updated: 2024/06/29 13:33:18 by ankoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *big, const char *little, size_t len)
+char	*ft_strdup(const char *s)
 {
-	size_t	b;
-	size_t	l;
 
-	b = 0;
-	if (!*little)
-		return ((char *)big);
-	while (b < len && big[b])
-	{
-		l = 0;
-		while (big[b + l] && little[l] \
-				&& big[b + l] == little[l] && b + l < len)
-		{
-			l++;
-			if (!little[l])
-				return ((char *)big + b);
-		}
-		b++;
-	}
-	return (NULL);
 }
